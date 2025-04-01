c9cb44ce94c65b383d8e62c7b5a9528d2d27035a
swu-hyk <jh0321forever@gmail.com>
2025-04-01 18:51:48 +0800

    1

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -8,10 +8,8 @@
 #[allow(unused_variables, unused_assignments)]
 fn main() {
     let my_option: Option<()> = None;
-    if my_option.is_some() {
-    }
 
-    let my_arr = &[
+    let my_arr = [
         -1, -2, -3,
         -4, -5, -6
     ];
